396c57e326b47e0fc42ddcb97b3df72d3a82b099
Halil Hakan Karabay <hakankarabay@icloud.com>
2021-12-15 03:09:51 +0300

    defer keyword understood.

--- defer/main.go
+++ defer/main.go
@@ -1,8 +1,21 @@
 package main
 
-import f "fmt"
+import . "fmt"
 
 func main() {
-	defer f.Println("first")
-	f.Println("second")
+	//defer f.Println("first 1")
+	//defer f.Println("first 2")
+	//defer f.Println("first 3")
+	//defer f.Println("first 4")
+	//defer f.Println("first 5")
+	//f.Println("second")
+
+	deger := 0
+	for deger < 10 {
+		defer Println(deger) // its bad idea btw.
+		deger++
+	}
+
+	Println("After for loop")
+
 }
